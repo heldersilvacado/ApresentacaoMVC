b3872c0138e855a515eb92c99e7aab1486b25410
heldersilvamvc <heldersilva.cado@gmail.com>
2020-03-24 21:00:25 -0300

    Apresentação do HomeController apresentando formulario para calcular dados de um formulário

---
+++ WebAnalista/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using WebAnalista.Models;
+
+namespace WebAnalista.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            Produtos produtos = new Produtos();
+            
+            return View(produtos);
+        }
+
+        [HttpPost]
+        public IActionResult Index([FromForm] Produtos produtos)
+        {
+            string recebeNome = Request.Form["nome"];
+            string recebeCategoria = Request.Form["categoria"];
+            double recebePreco = double.Parse(Request.Form["preco"]);
+            double recebeAumento = double.Parse(Request.Form["aumento"]);
+            int recebeQuantidade = int.Parse(Request.Form["quantidade"]);
+            int recebeSaida = int.Parse(Request.Form["saida"]);
+            double venda = double.Parse(Request.Form["venda"]);
+            double recebeValorDoEstoque = double.Parse(Request.Form["valorDoEstoque"]);
+          
+            //preencher os campos do Model
+
+            produtos.Nome = recebeNome.Trim().ToUpper();
+            produtos.Categoria = recebeCategoria.Trim().ToUpper();
+            produtos.Preco = double.Parse(recebePreco.ToString());
+            produtos.Aumento = double.Parse(recebeAumento.ToString());
+            produtos.Quantidade = recebeQuantidade;
+            produtos.QuantidadeSaida = recebeSaida;
+
+            //calculo dos Métodos
+
+            produtos.CalcularVenda();
+            produtos.CalcularValorDoEstoque();
+            produtos.ResultadoEstoque = produtos.SaidaEstoque(produtos.QuantidadeSaida);
+            
+            return View(produtos);
+        }
+    }
+}
---
+++ WebAnalista/Models/Produtos.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAnalista.Models
+{
+    public class Produtos
+    {
+        //atributos da classe
+        [DisplayName("Nome")]
+        [Required(ErrorMessage ="Você deve entrar com um nome para o produto")]
+        public string Nome {get; set; }
+
+        [DisplayName("Categoria")]
+        [Required(ErrorMessage = "Você deve entrar com um nome para a Categoria")]
+        public string Categoria { get; set; }
+
+        [DisplayName("Preco")]
+        [Required(ErrorMessage = "Você deve entrar com o preço do produto")]
+        public double Preco { get; set; }
+
+        [DisplayName("Aumento")]
+        [Range(0, 999999.99)]
+        [Required(ErrorMessage = "Você deve entrar com o aumento do produto")]
+        public double Aumento { get; set; }
+
+        [DisplayName("Venda")]
+        [Range(0, 999999.99)]
+        [Required(ErrorMessage = "Você deve entrar com a venda do produto")]
+        public double Venda { get; set; }
+
+        [DisplayName("Quantidade")]
+        [Range(0,999999.99)]
+        [Required(ErrorMessage = "Você deve entrar com a quantidade do produto")]
+        public int Quantidade { get;  set; }
+
+        [DisplayName("QuantidadeEntrada")]
+        [Range(0, 999999.99)]
+        [Required(ErrorMessage = "Você deve entrar com a quantidade do produto")]
+        public int QuantidadeEntrada { get; set; }
+
+        [DisplayName("QuantidadeSaida")]
+        [Range(0, 999999.99)]
+        [Required(ErrorMessage = "Você deve entrar com a quantidade do produto")]
+        public int QuantidadeSaida { get; set; }
+
+        public int ResultadoEstoque { get; set; }
+        //Contrutores das Classes
+        public Produtos()
+        {
+        }
+
+        public Produtos(string nome, string categoria, double preco, double aumento, double venda, int quantidade, int quantidadeEntrada, int quantidadeSaida)
+        {
+            Nome = nome;
+            Categoria = categoria;
+            Preco = preco;
+            Aumento = aumento;
+            Venda = venda;
+            Quantidade = quantidade;
+            QuantidadeEntrada = quantidadeEntrada;
+            QuantidadeSaida = quantidadeSaida;
+        }
+
+        //métodos da Classe
+
+        public double CalcularVenda()
+        {
+            double venda;
+            venda = Preco*(Aumento/100)+Preco;
+            return venda;
+        }
+        
+        public double CalcularValorDoEstoque()
+        {
+            return (Preco * (Aumento / 100) + Preco)*Quantidade;
+        }
+
+        public int EntradaEstoque(int QuantidadeEntrada)
+        {
+            return Quantidade += QuantidadeEntrada;
+        }
+
+        public int SaidaEstoque(int QuantidadeSaida)
+        {
+            return Quantidade -= QuantidadeSaida;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", R$ " + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                   + ", temos " + Quantidade + " unidades. "
+                   + " O Valor do Estoque é de: " + CalcularValorDoEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
